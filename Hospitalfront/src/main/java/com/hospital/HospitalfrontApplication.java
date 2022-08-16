f5ec27a9768ed8257ff9cb1651128ed76dd27094
12345shobana <109730025+12345shobana@users.noreply.github.com>
2022-08-16 14:51:19 +0530

    Hospital management system
    The hospital management system helps register complete patient information. It captures and stores the medical history, treatment required, details of their previous visits, upcoming appointments if any, reports, insurance details and more. It helps eliminate the need to get these details on every visit.

---
+++ Hospitalfront/src/main/java/com/hospital/HospitalfrontApplication.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalfrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalfrontApplication.class, args);
+	}
+
+}
---
+++ Hospitalfront/src/test/java/com/hospital/HospitalfrontApplicationTest.java
@@ -0,0 +1,14 @@
+package com.hospital;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HospitalfrontApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
